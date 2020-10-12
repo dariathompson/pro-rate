529189623f8d36ff914cc7a0b4b9ab341c90dcc7
rafahg <r.hgar78@gmail.com>
2020-10-12 12:30:53 +0100

    mongodb installed and active, starting setup

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
+const db = require('./db')
+
 const app = express()
 const apiPort = 5000
 
@@ -8,8 +11,10 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors())
 app.use(bodyParser.json())
 
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send("Let's modify this!, lovely!!!!!!, It is working, what a feeling!!!!!!!!")
 })
 
 app.listen(apiPort, () => console.log(`Server running at localhost:${apiPort}`))
